913641a26b23dcc8daf45f256c9e5b36b670ce1d
jason yang <jhyapril@gmail.com>
2018-07-06 14:03:29 +0800

    added components

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render() {
+    return(
+      <div className="jumbotron">
+        <h1>Course Management Center</h1>
+        <p> This website displays and manages good online course videos. It is developed based on React & Redux. </p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
