23367db64820bc392c3c69657d31ee547f650773
jason yang <jhyapril@gmail.com>
2018-07-06 14:03:20 +0800

    updated src files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import './styles/index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+render(
+  <Router browserHistory={browserHistory} routes={routes}/>,
+  document.getElementById('app')
+);
+
