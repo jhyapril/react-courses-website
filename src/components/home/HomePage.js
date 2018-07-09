eca09c0c5ece61391a7b8c2be5d0611086bdfc8c
jason yang <jhyapril@gmail.com>
2018-07-09 21:12:28 +0800

    added course page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class HomePage extends React.Component {
+
   render() {
     return(
       <div className="jumbotron">
         <h1>Course Management Center</h1>
         <p> This website displays and manages good online course videos. It is developed based on React & Redux. </p>
+        <Link to="about" className="btn btn-info">Know more</Link>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import HomePage from "./components/home/HomePage";
 import App from "./components/App";
+import AboutPage from "./components/about/AboutPage";
+import CoursePage from "./components/course/CoursePage";
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
+    <Route path="about" component={AboutPage}/>
+    <Route path="course" component={CoursePage}/>
   </Route>
 );
