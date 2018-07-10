625adb7fdc1150791bdec7315302a632801f6ac3
jason yang <jhyapril@gmail.com>
2018-07-11 01:12:52 +0800

    added new actions update & create

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,10 +5,29 @@ function loadCoursesSuccess(allCourses) {
   return {type: types.LOAD_COURSES_SUCCESS, payload: {allCourses}};
 }
 
+export function updateCourseSuccess(courseId, updatedCourse) {
+  return {type: types.UPDATE_COURSES_SUCCESS, payload: {courseId, updatedCourse}};
+}
+
+export function createCourseSuccess(newCourse) {
+  return {type: types.CREATE_COURSES_SUCCESS, payload: {newCourse}};
+}
+
+
 export function loadCourses() {
   return (dispatch) => {
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return (dispatch, getState) => {
+    return courseApi.saveCourse(course).then(resp => {
+      course.id ? dispatch(updateCourseSuccess(course.id, resp)) : dispatch(createCourseSuccess(resp));
     }).catch(error => {
       throw(error);
     });
